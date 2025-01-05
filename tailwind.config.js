adc6b52a57e1688a97e772bb0e4ee3c20cc209c1
herukan <heru842@gmail.com>
2025-01-06 00:25:32 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  // darkMode: "class",
-  darkMode: 'false',
+  darkMode: "class",
+  // darkMode: 'false',
   theme: {
     screens: {
       mob: "375px",
